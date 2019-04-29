a5bf013f2d0190805246bd9f264ee7ff7cdd816f
Mukesh Ghimire <samu.mg60@gmail.com>
2019-04-30 04:45:05 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
-    "mongoUrl": "mongodb://localhost:27017/careerpad",
-=======
     "mongoUrl": "mongodb+srv://careerpadadmin:X3K3RbsJbv8tfsyv@mflix-mcn8v.gcp.mongodb.net/careerpad?retryWrites=true",
->>>>>>> a04d58a86a21513248e07f8d89b33a989eb76e14
 }
