a9573979323f1e676c31cb191407ba2b86549e8d
Mukesh Ghimire <samu.mg60@gmail.com>
2019-05-04 11:49:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/uploads',express.static(path.join(__dirname, '../uploads')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/uploads',express.static(path.join(__dirname, '/uploads')));
 
 //session config
 app.use(session({
