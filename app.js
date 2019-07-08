c26ff8653ddaf4016d71ab133ed1509785a5afbb
Mukesh Ghimire <samu.mg60@gmail.com>
2019-07-08 10:58:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('admin/page-error');
+  res.render('/admin/page-error');
 });
  
 module.exports = app;
